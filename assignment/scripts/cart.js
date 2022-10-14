53e4d402ae9afa0ac6316ee570f883106709d4b9
August McAllister <august.chm@gmail.com>
2022-10-14 11:03:45 -0500

    Added functions, testing to follow

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,17 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+var basket = [];
+
+const addItem = (item) => {
+  basket.push(item);
+  return true;
+}
+
+const listItems = () => {
+  for (item of basket) {
+    console.log(item);
+  }
+}
+
+const empty = () => { basket = [] };
